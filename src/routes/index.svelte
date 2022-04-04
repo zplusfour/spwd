a05e3ab229c866878e2311fe441971614b8bffd9
zplusfour <zgjoniur@gmail.com>
2022-04-04 14:37:55 +0300

    hashing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,12 @@
 <div>
-  <form action="/api/add" method="post">
-    <input name="app" id="app" type="text" placeholder="app name" />
-    <input name="password" id="password" type="password" placeholder="password (we dont store any passwords)" />
-    <input type="submit" value="Add" />
-  </form>
+	<form action="/api/add" method="post">
+		<input name="app" id="app" type="text" placeholder="app name" />
+		<input
+			name="password"
+			id="password"
+			type="password"
+			placeholder="password"
+		/>
+		<input type="submit" value="Add" />
+	</form>
 </div>
