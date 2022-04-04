d9436293c76a05221fa843c26227823c113e7d50
zplusfour <zgjoniur@gmail.com>
2022-04-04 14:20:10 +0300

    ez form data

--- src/routes/api/add.ts
+++ src/routes/api/add.ts
@@ -1,19 +1,30 @@
-import type { RequestHandler } from "@sveltejs/kit";
+import type { RequestHandler } from '@sveltejs/kit';
+import md5 from 'md5';
 
 export const post: RequestHandler = async ({ request }) => {
-  const data = await request.formData();
+	const data: any = await request.formData();
+	const form = Object([...data.entries()]);
+	const app = form[0][1];
+	const password = form[1][1];
+	const encApp = md5(app);
+	const encPassword = md5(password);
 
-  return {
-    body: {
-      data
-    }
-  } as any;
-}
+	return {
+		status: 200,
+		body: {
+			app: encApp,
+			password: encPassword,
+			rApp: app,
+			rPassword: password
+		}
+	};
+};
 
-export const get: RequestHandler = ({ request }) => {
-  return {
-    body: {
-      test: true
-    }
-  } as any;
+export const get: RequestHandler = ({ url }) => {
+	return {
+		body: {
+			v: url,
+			test: true
+		}
+	} as any;
 };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
 <div>
-  
+  <form action="/api/add" method="post">
+    <input name="app" id="app" type="text" placeholder="app name" />
+    <input name="password" id="password" type="password" placeholder="password (we dont store any passwords)" />
+    <input type="submit" value="Add" />
+  </form>
 </div>
