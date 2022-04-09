618cbfe5f32327c37aa45668d06147785fc4879c
zplusfour <zgjoniur@gmail.com>
2022-04-09 14:42:48 +0300

    readme + required input

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # spwd - a website that stores passwords
 
-### configuration
-add a pwds.json file to src/routes/api with the content: `[]`
-in this file, your passwords will be stored in a json array
+now it automatically writes a pwds file if you dont have one lol
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,12 +37,13 @@
 </script>
 <div>
 	<form action="/api/add" method="post">
-		<input name="app" id="app" type="text" placeholder="app name" />
+		<input name="app" id="app" type="text" placeholder="app name" required />
 		<input
 			name="password"
 			id="password"
 			type="password"
 			placeholder="password"
+			required
 		/>
 		<input type="submit" value="Add" />
 	</form>
